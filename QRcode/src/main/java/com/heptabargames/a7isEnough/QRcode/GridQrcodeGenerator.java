8675105162e84a3ce59087fd1385fa1af8ae5cb9
loicsrz <ljedi007@gmail.com>
2018-04-25 11:17:35 +0200

    Add modification to QRCodeGenerator

--- QRcode/src/main/java/com/heptabargames/a7isEnough/QRcode/GridQrcodeGenerator.java
+++
@@ -1,4 +0,0 @@
-package com.heptabargames.a7isEnough.QRcode;
-
-public class GridQrcodeGenerator {
-}
--- QRcode/src/main/java/com/heptabargames/a7isEnough/QRcode/QRcodeGenerator.java
+++ QRcode/src/main/java/com/heptabargames/a7isEnough/QRcode/QRcodeGenerator.java
@@ -1,53 +1,55 @@
 package com.heptabargames.a7isEnough.QRcode;
 
+
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import net.glxn.qrgen.android.QRCode;
+import net.glxn.qrgen.core.image.ImageType;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
+public class QRcodeGenerator {
 
-public class SimpleQrcodeGenerator {
 
-    private static BitMatrix generateMatrix(final String data, final int size) throws WriterException {
-        final BitMatrix bitMatrix = new QRCodeWriter().encode(data, BarcodeFormat.QR_CODE, size, size);
-        return bitMatrix;
-    }
+    File file = QRCode.from("Hello World").file();
+    // get QR stream from text using defaults
+    ByteArrayOutputStream stream = QRCode.from("Hello World").stream();
 
-    private static void writeImage(final String outputFileName, final String imageFormat, final BitMatrix bitMatrix) throws FileNotFoundException,
-            IOException {
-        final FileOutputStream fileOutputStream = new FileOutputStream(new File(outputFileName));
-        MatrixToImageWriter.writeToStream(bitMatrix, imageFormat, fileOutputStream);
-        fileOutputStream.close();
-    }
+    static OutputStream GenerateQRcode() {
+        // override the image type to be JPG
+        QRCode.from("Hello World").to(ImageType.JPG).file();
+        QRCode.from("Hello World").to(ImageType.JPG).stream();
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        System.out.println("SimpleQrcodeGenerator DEBUT");
+        // override image size to be 250x250
+        QRCode.from("Hello World").withSize(250, 250).file();
+        QRCode.from("Hello World").withSize(250, 250).stream();
 
-        try {
-            final String data = "Test";
-            final String imageFormat = "png";
-            final String outputFileName = "c:/code/qrcode-01." + imageFormat;
-            final int size = 400;
+        QRCode.from("Hello World").withColor(0xFFFF0000, 0xFFFFFFAA).file();
 
-            // encode
-            final BitMatrix bitMatrix = generateMatrix(data, size);
+        // supply own file name
+        QRCode.from("Hello World").file("QRCode");
 
-            // write in a file
-            writeImage(outputFileName, imageFormat, bitMatrix);
+        // supply charset hint to ZXING
+        QRCode.from("Hello World").withCharset("UTF-8");
 
-            System.out.println("SimpleQrcodeGenerator FIN");
+// supply error correction level hint to ZXING
+        QRCode.from("Hello World").withErrorCorrection(ErrorCorrectionLevel.L);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+// supply any hint to ZXING
+        QRCode.from("Hello World").withHint(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+        OutputStream outputStream = null;
+
+        // supply own outputstream
+        QRCode.from("Hello World").to(ImageType.PNG).writeTo(outputStream);
+
+        return outputStream;
     }
 }
+
+
 
