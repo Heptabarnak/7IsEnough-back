545eaa0499c6424cac231f46fe341c277f1234bd
loicsrz <ljedi007@gmail.com>
2018-04-27 09:03:40 +0200

    Add method for the token

--- QRcode/src/main/java/com/heptabargames/a7isEnough/QRcode/CreateQRCode.java
+++ QRcode/src/main/java/com/heptabargames/a7isEnough/QRcode/CreateQRCode.java
@@ -7,6 +7,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 import static com.skrymer.qrbuilder.decorator.ImageOverlay.*;
 import static com.skrymer.qrbuilder.decorator.ColoredQRCode.colorizeQRCode;
@@ -18,13 +20,21 @@ public class CreateQRCode {
         public static final int WIDTH = 260;
         public static final int HEIGHT = 260;
 
+        public static void GenerateToken() throws NoSuchAlgorithmException {
+            byte[] bytes = new byte[20];
+            SecureRandom.getInstanceStrong().nextBytes(bytes);
+
+
+        }
+
+
         public static void main(String[] args) throws Exception {
             QRCode.ZXingBuilder.build(builder ->
                     builder.withSize(WIDTH, HEIGHT)
                             .and()
                             .withData("Il est née le divin enfant")
                             .and()
-                            .withDecorator(colorizeQRCode(Color.blue.darker()))
+                            .withDecorator(colorizeQRCode(Color.black.darker()))
                             .and()
                             .withDecorator(addImageOverlay(readImage("QRcode/src/main/resources/Logo_seul_blanc.png"), TRANSPARENCY, OVERLAY_RATIO))
                             .and()
