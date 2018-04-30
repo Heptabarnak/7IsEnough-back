4c844ef822caf182f814311cc053aa24287b519c
loicsrz <ljedi007@gmail.com>
2018-04-30 09:01:27 +0200

    Add interface to generate QRcode

---
+++ QRcode/src/main/java/com/heptabargames/a7isEnough/QRcode/QrCode.java
@@ -0,0 +1,9 @@
+package com.heptabargames.a7isEnough.QRcode;
+
+import javax.swing.*;
+
+public class QrCode {
+    private JButton generateButton;
+    private JButton saveInButton;
+    private JTextArea generateQrCodeAndSaveTextArea;
+}
