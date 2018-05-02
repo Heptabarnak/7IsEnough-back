99bcfbcf8ec66e613ebe1720621ea7bcf5b83224
Marc-Antoine Fernandes <embraser01@gmail.com>
2018-05-02 17:37:24 +0200

    Fix QR Generation

--- QRcode/src/main/java/com/heptabargames/a7isEnough/QRcode/CreateQRCode.java
+++ QRcode/src/main/java/com/heptabargames/a7isEnough/QRcode/CreateQRCode.java
@@ -1,86 +1,81 @@
 package com.heptabargames.a7isEnough.QRcode;
 
 import com.skrymer.qrbuilder.QRCode;
-import de.mkammerer.argon2.Argon2;
-import de.mkammerer.argon2.Argon2Factory;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
-import static com.skrymer.qrbuilder.decorator.ImageOverlay.*;
 import static com.skrymer.qrbuilder.decorator.ColoredQRCode.colorizeQRCode;
+import static com.skrymer.qrbuilder.decorator.ImageOverlay.addImageOverlay;
 
 public class CreateQRCode {
 
-        public static final float TRANSPARENCY = 1f;
-        public static final float OVERLAY_RATIO = 0.25f;
-        public static final int WIDTH = 260;
-        public static final int HEIGHT = 260;
+    private static final float TRANSPARENCY = 1f;
+    private static final float OVERLAY_RATIO = 0.25f;
+    private static final int WIDTH = 260;
+    private static final int HEIGHT = 260;
 
-        public static String GenerateToken() throws NoSuchAlgorithmException {
-            byte[] bytes = new byte[20];
-            SecureRandom.getInstanceStrong().nextBytes(bytes);
-            String token = Base64.getEncoder().encodeToString(bytes);
-            String hash = null;
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA-512");
-                byte[] bytes1 = md.digest(token.getBytes(StandardCharsets.UTF_8));
-                StringBuilder sb = new StringBuilder();
-                for (byte aByte : bytes){
-                    sb.append(Integer.toString((aByte & 0xff)+ 0x100,16).substring(1));
-                }
-                hash = sb.toString();
-
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-
-            }
-            GenerateQRcode(token);
-            return hash;
-
-        }
-        public static void GenerateQRcode (String token){
-            QRCode.ZXingBuilder.build(builder ->
-                    builder.withSize(WIDTH, HEIGHT)
-                            .and()
-                            .withData(token)
-                            .and()
-                            .withDecorator(colorizeQRCode(Color.magenta.darker()))
-                            .and()
-                            .withDecorator(addImageOverlay(readImage("QRcode/src/main/resources/Logo_seul(1).png"), TRANSPARENCY, OVERLAY_RATIO))
-                            .and()
-                            .verify(true)
-
-            ).toFile("./qrCode.png", "PNG");
-        }
-
-
-        public static String generateQRcode() {
-
+    private static String GenerateToken() throws NoSuchAlgorithmException {
+        byte[] bytes = new byte[20];
+        SecureRandom.getInstanceStrong().nextBytes(bytes);
+        String token = Base64.getEncoder().encodeToString(bytes);
         String hash = null;
         try {
-            hash = GenerateToken();
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] hashBytes = md.digest(token.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for (byte aByte : hashBytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+            }
+            hash = sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+
+        }
+        generateImage(token);
+        return hash;
+
+    }
+
+    public static void generateImage(String token) {
+        QRCode.ZXingBuilder.build(builder ->
+                builder.withSize(WIDTH, HEIGHT)
+                        .and()
+                        .withData(token)
+                        .and()
+                        .withDecorator(colorizeQRCode(Color.magenta.darker()))
+                        .and()
+                        .withDecorator(addImageOverlay(readImage("QRcode/src/main/resources/Logo_seul(1).png"), TRANSPARENCY, OVERLAY_RATIO))
+                        .and()
+                        .verify(true)
+
+        ).toFile("./qrCode.png", "PNG");
+    }
+
+
+    public static String generateQRCode() {
+        try {
+            return GenerateToken();
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-            return hash;
+        return null;
+    }
 
+    private static BufferedImage readImage(String path) {
+        try {
+            return ImageIO.read(new File(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        public static BufferedImage readImage(String path) {
-            try {
-                return ImageIO.read(new File(path));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    }
 }
